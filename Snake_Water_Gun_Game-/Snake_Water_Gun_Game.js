9d94d14ea4a7b266d0edf371fa23f58a6bb7c2ce
Yash Varpe <104847857+YashVarpe05@users.noreply.github.com>
2023-02-26 15:17:46 +0530

    adding comments for better under standing

--- Snake_Water_Gun_Game-/Snake_Water_Gun_Game.js
+++ Snake_Water_Gun_Game-/Snake_Water_Gun_Game.js
@@ -1,28 +1,39 @@
+// Sync the prompt to use prompt in vs code
 const prompt = require("prompt-sync")();
 
 console.log("\n \t ***** WELCOME TO THE SNAKE - WATER - GUN *****");
+// winning condition of the game
 // Snake =water
 // water =gun
 // gun=snake
+
+// declaration-section
+
 let options = ["S", "G", "W"];
 let comp;
 let comp_index;
+// no of round user want to play
 let round_count = Number.parseInt(
 	prompt(`\n Enter the no of round you want to play`, `10`)
 );
+
 let round = 0;
 let user_score = 0;
 let comp_score = 0;
 let count = 0;
 let user;
 
+// a loop till the no of round user have to play
 while (round < round_count) {
 	user = prompt("\n Enter 'S' for Snake  'W' for Water  'G' for Gun ");
+
+	// convert character into upper case
 	user = user.toUpperCase();
 	comp_index = Math.floor(Math.random() * options.length);
 	comp = options[comp_index];
-
+	// condition checking if user choose S w G the go inside another loop else invalid condition
 	if (user == "S" || user == "W" || user == "G") {
+		// compering user option and computer option
 		if (
 			(user == "S" && comp == "W") ||
 			(user == "G" && comp == "S") ||
@@ -51,8 +62,11 @@ while (round < round_count) {
 	} else {
 		console.log(` You Enter Invalid Option `);
 	}
+	// end of loop
 }
+// printing score of user and computer
 console.log(`Your Score ${user_score} & Computer Score ${comp_score} `);
+// checking the winner
 if (user_score > comp_score) {
 	console.log(`\n Congrats, You Beat Your Computer!`);
 } else if (user_score < comp_score) {
@@ -60,3 +74,5 @@ if (user_score > comp_score) {
 } else {
 	console.log(`\n Good Try, You & Your Computer Both Make A Tie!`);
 }
+/* Thank you */
+
